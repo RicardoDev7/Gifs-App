a446e89613be187954e53161fd85951f7cdadad8
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-11 22:43:45 -0500

    feat: separacion en componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,17 +4,23 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     loadComponent: () =>
-       import('./gifs/pages/dashboard-page/dashboard-page')
-  },
-  {
-    path: 'trending',
-    loadComponent: () =>
-       import('./gifs/pages/trending-page/trending-page')
-  },
-  {
-    path: 'search',
-    loadComponent: () =>
-       import('./gifs/pages/search-page/search-page')
+       import('./gifs/pages/dashboard-page/dashboard-page'),
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () =>
+          import('./gifs/pages/trending-page/trending-page')
+      },
+      {
+        path: 'search',
+        loadComponent: () =>
+          import('./gifs/pages/search-page/search-page')
+      },
+      {
+        path: '**',
+        redirectTo: 'trending'
+      }
+    ]
   },
   {
     path: '**',
