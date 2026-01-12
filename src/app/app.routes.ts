8e82a6476070fa9596f2d4d9e20f36b9861768af
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-12 00:31:25 -0500

    feat: agregar path default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard'
+  },
+  {
     path: 'dashboard',
     loadComponent: () =>
        import('./gifs/pages/dashboard-page/dashboard-page'),
