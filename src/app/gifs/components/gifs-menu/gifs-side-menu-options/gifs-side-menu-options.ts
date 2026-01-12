91f50e86075a205a74eb943e114807d481dc0123
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-11 23:00:17 -0500

    feat: navegacion y menu dinamico

--- src/app/gifs/components/gifs-menu/gifs-side-menu-options/gifs-side-menu-options.ts
+++ src/app/gifs/components/gifs-menu/gifs-side-menu-options/gifs-side-menu-options.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOption {
+  label: string;
+  sublabel: string;
+  router: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-options.html',
 })
-export class GifsSideMenuOptions { }
+export class GifsSideMenuOptions {
+  menuOptions: MenuOption[] = [
+    {
+      label: 'Trending',
+      sublabel: 'Gifs más populares',
+      router: '/dashboard/trending',
+      icon: 'fa-solid fa-chart-line'
+    },
+    {
+      label: 'Buscador',
+      sublabel: 'Buscar gifs',
+      router: '/dashboard/search',
+      icon: 'fa-solid fa-magnifying-glass'
+    }
+  ]
+}
