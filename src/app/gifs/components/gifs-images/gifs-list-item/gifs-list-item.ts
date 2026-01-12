94cc65441bcc80734951ef045caea44d358afbc6
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-11 23:25:54 -0500

    feat: componentes de imagenes

---
+++ src/app/gifs/components/gifs-images/gifs-list-item/gifs-list-item.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gifs-list-item',
+  imports: [],
+  templateUrl: './gifs-list-item.html'
+})
+export class GifsListItem { }
---
+++ src/app/gifs/components/gifs-images/gifs-list/gifs-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { GifsListItem } from "../gifs-list-item/gifs-list-item";
+
+@Component({
+  selector: 'app-gifs-list',
+  imports: [GifsListItem],
+  templateUrl: './gifs-list.html'
+})
+export class GifsList { }
